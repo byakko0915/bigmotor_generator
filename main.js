c381156a35b736888a8421f6db0380eb9155e412
byakko0915 <hyh2007@live.jp>
2023-08-06 18:49:44 +0900

    繰り返す回数を指定できるようにした

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 function inputCheck() {
     var inputValue1 = document.getElementById("word1").value;
     var inputValue2 = document.getElementById("word2").value;
-    document.getElementById("output").innerHTML = inputValue1.repeat(18) + inputValue2.repeat(9) + inputValue1.repeat(18)
+    document.getElementById("output").innerHTML = inputValue1.repeat(number1.value) + inputValue2.repeat(number2.value) + inputValue1.repeat(number1.value)
   }
+
+let number1 = document.getElementById('number1');
+let number2 = document.getElementById('number2');
+number1.addEventListener('input', inputCheck);
+number2.addEventListener('input', inputCheck);
